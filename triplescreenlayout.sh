7a531eae3a0d675459bf9af44177697f6f739bf6
Dummy <duminaztricktech@gmail.com>
2017-04-05 17:21:56 -0400

    screenlayout script
    arandr generated script

---
+++ triplescreenlayout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --off --output LVDS-0 --mode 1920x1080 --pos 1360x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 3280x0 --rotate normal --output DP-2 --off --output DP-1 --mode 1360x768 --pos 0x0 --rotate normal --output DP-0 --off
---
+++ triplescreenlayout_VGA.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --mode 1024x768 --pos 0x0 --rotate normal --output LVDS-0 --mode 1920x1080 --pos 1024x0 --rotate normal --output HDMI-0 --primary --mode 1920x1080 --pos 2944x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-0 --off
